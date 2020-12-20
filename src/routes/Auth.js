cf9ab4f0eb9a7898126fb497bafbf59c5f7de86b
5tarlight <developerstarlight@gmail.com>
2020-12-20 21:26:54 +0900

    init router and routes

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
+import { authService } from "../fbase";
 
 const Auth = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [newAccount, setNewAccount] = useState(true)
   const onChange = e => {
     const { target: { name, value } } = e
     if (name === 'email') {
@@ -11,8 +13,21 @@ const Auth = () => {
       setPassword(value)
     }
   }
-  const onSubmit = event => {
+  const onSubmit = async event => {
     event.preventDefault()
+
+    try {
+      if (newAccount) {
+        const data = await authService.createUserWithEmailAndPassword(email, password)
+        console.log(data)
+      } else {
+        // Log in
+        const data = await authService.signInWithEmailAndPassword(email, password)
+        console.log(data)
+      }
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
@@ -34,7 +49,7 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type='submit' value='Log In'/>
+        <input type='submit' value={newAccount ? 'Create Account' : 'Log in'}/>
       </form>
 
       <div>
