9fcedab0a2e8f152fe14ee624afea181d0b0807b
5tarlight <developerstarlight@gmail.com>
2020-12-20 22:09:21 +0900

    LogOut

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,22 +2,28 @@ import React, { Fragment } from 'react'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import Auth from '../routes/Auth'
 import Home from '../routes/Home'
+import Navigation from "./Navigation";
+import Profile from "../routes/Profile";
 
 const Router = ({ isLoggedIn }) => {
 
   return (
     <HashRouter>
+      {isLoggedIn && <Navigation/>}
       <Switch>
-        { isLoggedIn ?
-        <Fragment>
+        {isLoggedIn ?
+          <Fragment>
+            <Route exact path='/'>
+              <Home/>
+            </Route>
+            <Route exact path='/profile'>
+              <Profile/>
+            </Route>
+          </Fragment>
+          :
           <Route exact path='/'>
-            <Home />
-          </Route>
-        </Fragment>
-         :
-         <Route exact path='/'>
-           <Auth />
-          </Route> }
+            <Auth/>
+          </Route>}
       </Switch>
     </HashRouter>
   )
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { authService } from "../fbase";
+import { useHistory } from 'react-router-dom'
 
-const Profile = () => <span>Profile</span>
+const Profile = () => {
+  const history = useHistory()
+
+  const onLogOutClick = () => {
+    authService.signOut()
+    history.push('/')
+  }
+
+  return (
+    <Fragment>
+      <button onClick={onLogOutClick}>Logout</button>
+      <span>Profile</span>
+    </Fragment>
+  )
+}
 
 export default Profile
