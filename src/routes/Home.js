e4ebe0e5217c70e937fa03f06cd8728352c1daa4
5tarlight <developerstarlight@gmail.com>
2020-12-20 23:09:06 +0900

    loading tweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { dbService } from "../fbase";
 
 const Home = () => {
   const [nweet, setNweet] = useState('')
+  const [nweets, setNweets] = useState([])
+
+  const getNweets = async () => {
+    const nwts = await dbService.collection('nweets').get()
+    nwts.forEach(doc => {
+      const nweetObject = {
+        ...doc.data(),
+        id: doc.id
+      }
+      setNweets(prev => [nweetObject, ...prev])
+    })
+  }
+
+  useEffect(() => {
+    getNweets()
+  }, [])
+
   const onSubmit = async event => {
     event.preventDefault()
     if (nweet === '') return
@@ -19,12 +36,22 @@ const Home = () => {
     setNweet(value)
   }
 
-  return (<div>
-    <form onSubmit={onSubmit}>
-      <input value={nweet} onChange={onChange} type={'text'} placeholder={`What's on your mind?`} maxLength={120}/>
-      <input type={'submit'} value={'Nweet'}/>
-    </form>
-  </div>)
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input value={nweet} onChange={onChange} type={'text'} placeholder={`What's on your mind?`} maxLength={120}/>
+        <input type={'submit'} value={'Nweet'}/>
+      </form>
+      <div>
+        {nweets.map((nweet, i) => (
+          <div key={i}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Home
